24d28e5f2a6ece49c9355b1f2f0d8639a7f50684
Hans Vos <hans.vos@codeimmersives.com>
2020-03-12 15:59:31 -0400

    fixed code

--- app.js
+++ app.js
@@ -60,6 +60,9 @@ app.use(
   })
 );
 
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.errors = req.flash('error');
